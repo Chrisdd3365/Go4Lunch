abc4374b7ce498ad85b53f6b4fdd5cab22de1e29
Christophe DURAND <cdurand.dev@gmail.com>
2020-10-06 21:14:39 +0200

    Showing all restaurants nearby user current location, using google places SDK

---
+++ app/src/main/java/com/christophedurand/go4lunch/ui/listView/MyItemRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.christophedurand.go4lunch.ui.listView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.christophedurand.go4lunch.R;
+import com.christophedurand.go4lunch.ui.listView.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+
+    public MyItemRecyclerViewAdapter(List<DummyItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_list_view_content, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.restaurant_name);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
--- app/src/main/java/com/christophedurand/go4lunch/ui/mapView/MapFragment.java
+++ app/src/main/java/com/christophedurand/go4lunch/ui/mapView/MapFragment.java
@@ -3,6 +3,10 @@ package com.christophedurand.go4lunch.ui.mapView;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
@@ -11,14 +15,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.christophedurand.go4lunch.R;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -27,28 +26,29 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.PlaceLikelihood;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-import utils.GooglePlacesCalls;
-import utils.NetworkAsyncTask;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
-public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback, NetworkAsyncTask.Listeners {
+public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback {
 
     //-- PROPERTIES
-    String apiKey = "AIzaSyB8G6oFNA-nM5rPVKk6VqPiqCOu1gFEdZY";
+    String apiKey = "AIzaSyC8HszJq_OD87OQUdjiqv5c67k-uw5B1bg";
 
     PlacesClient placesClient;
     // Use fields to define the data types to return.
@@ -58,6 +58,7 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
 
     private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap mMap;
+
     private Context context;
 
 
@@ -92,13 +93,13 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
         // Initialize the SDK
         Places.initialize(context.getApplicationContext(), apiKey);
 
         // Create a new PlacesClient instance
-        placesClient = Places.createClient(context);
+        placesClient = Places.createClient(requireActivity());
 
         return view;
     }
@@ -122,9 +123,8 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
                         // Logic to handle location object
                         LatLng currentLocation = new LatLng( location.getLatitude(), location.getLongitude() );
 
-                        mMap.addMarker(new MarkerOptions()
-                                .position(currentLocation)
-                                .title("YOU"));
+                        // Display a blue dot to represent the current user location
+                        mMap.setMyLocationEnabled(true);
 
                         // Move the camera instantly to current location with a zoom of 15.
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
@@ -132,7 +132,7 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
                         // Zoom in, animating the camera.
                         mMap.animateCamera(CameraUpdateFactory.zoomIn());
 
-                        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+                        // Construct a CameraPosition focusing on the view and animate the camera to that position.
                         CameraPosition cameraPosition = new CameraPosition.Builder()
                                 .target(currentLocation)    // Sets the center of the map to current Location
                                 .zoom(17)                   // Sets the zoom
@@ -141,7 +141,8 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
                                 .build();                   // Creates a CameraPosition from the builder
                         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-                        executeHttpRequest(location);
+                        // Display all restaurants near current user location
+                        getRestaurants(mMap);
 
                     }
                 });
@@ -167,28 +168,67 @@ public class MapFragment extends Fragment implements LocationListener, OnMapRead
 
     }
 
-    // ------------------
-    //  HTTP REQUEST
-    // ------------------
-    private void executeHttpRequest(Location location) {
-        GooglePlacesCalls.fetchNearbyRestaurants(context,
-                location.getLatitude() + "," + location.getLongitude());
+    private void getRestaurants(GoogleMap googleMap) {
+        // Use fields to define the data types to return.
+        List<Place.Field> placeFields = Arrays.asList(
+                Place.Field.NAME, Place.Field.ID,
+                Place.Field.LAT_LNG, Place.Field.TYPES
+        );
+
+        // Use the builder to create a FindCurrentPlaceRequest.
+        FindCurrentPlaceRequest request =
+                FindCurrentPlaceRequest.builder(placeFields).build();
+
+        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            placesClient.findCurrentPlace(request).addOnSuccessListener(((response) -> {
+                for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+
+                    Place place = placeLikelihood.getPlace();
+
+                    if (Objects.requireNonNull(place.getTypes()).contains(Place.Type.RESTAURANT)) {
+                        String restaurantName = place.getName();
+                        //String restaurantId = place.getId();
+                        LatLng restaurantLatLng = place.getLatLng();
+
+                        assert restaurantLatLng != null;
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(restaurantLatLng)
+                                .icon(bitmapDescriptorFromVector(requireActivity(), R.drawable.ic_restaurant_red_marker))
+                                .title(restaurantName)
+                                .snippet("test details snippet") );
+
+                    }
+                }
+            })).addOnFailureListener((exception) -> {
+                if (exception instanceof ApiException) {
+                    ApiException apiException = (ApiException) exception;
+                    Log.e("TAG Place not found", "Place not found: " + apiException.getStatusCode());
+                }
+            });
+        }
+//        else {
+//            // A local method to request required permissions;
+//            // See https://developer.android.com/training/permissions/requesting
+//            //getLocationPermission();
+//        }
     }
 
-    @Override
-    public void onPreExecute() {
+    //-- HELPER'S METHODS
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
 
+        assert vectorDrawable != null;
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        vectorDrawable.draw(canvas);
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
-    @Override
-    public void doInBackground() {
-
-    }
-
-    @Override
-    public void onPostExecute(String json) {
-
-    }
 
 
 }
